d71867415e6384b8f3e06204c9baaa6a00493ae3
Alex <alex3r@me.com>
2020-12-24 15:26:29 -0800

    added popup and local storage

--- content.js
+++ content.js
@@ -1,35 +1,68 @@
-const IDS = [
-    "dQw4w9WgXcQ",
-    "YddwkMJG1Jo",
-    "xvFZjo5PgG0",
-    "oHg5SJYRHA0",
-    "j8PxqgliIno",
-    "RvBwypGUkPo",
-    "pHhb4biR9k"
-];
-const image = chrome.extension.getURL("rick.jpg");
+$(() => {
 
-let matchLink = (url) => {
-    for (id of IDS) {
-        if (url.includes(id)) {
-            return true;
+    // youtube ids to seach for:
+    const IDS = [
+        "dQw4w9WgXcQ",
+        "YddwkMJG1Jo",
+        "xvFZjo5PgG0",
+        "oHg5SJYRHA0",
+        "j8PxqgliIno",
+        "RvBwypGUkPo",
+        "pHhb4biR9k"
+    ];
+    
+    // image to insert:
+    const image = chrome.extension.getURL("rick.jpg");
+    
+    // matches links to ids:
+    const matchLink = url => {
+        for (id of IDS) {
+            if (url.includes(id)) {
+                return true;
+            }
+        }
+    
+        return false;
+    }
+    
+    // inserts image next to matching links
+    const addImage = () => {
+        console.log("Inspecting page for Rick Rolls...");
+        let count = 0;
+    
+        $("a").filter(function() {
+            if (this.href && matchLink(this.href)) {
+                ++count;
+    
+                $(this).append(
+                    $(`<img class="__rickrolldetected" src="${image}" alt="Suspect Link" height="21px" />`)
+                );
+            }
+        });
+    
+        console.log(`Detected ${count} Rick Roll${count === 1 ? '' : 's'}`);
+    }
+    
+    // adds/removes images
+    const runSearch = on => {
+        if (on) {
+            addImage();
+        }
+        else {
+            $(".__rickrolldetected").each(function() {
+                $(this).remove();
+            })
         }
     }
 
-    return false;
-}
+    // on start up load state
+    chrome.storage.sync.get('state', ({ state }) => {
+        runSearch(state);
+    });
 
-console.log("Inspecting page for Rick Rolls...");
-let count = 0;
+    // listen to popup
+    chrome.runtime.onMessage.addListener(({ state }) => {
+        runSearch(state);
+    });
 
-$("a").filter(function() {
-    if (this.href && matchLink(this.href)) {
-        ++count;
-
-        $(this).append(
-            $(`<img src="${image}" alt="Suspect Link" height="21px" />`)
-        );
-    }
-});
-
-console.log(`Detected ${count} Rick Roll${count === 1 ? '' : 's'}`);
+})
---
+++ popup/popup.js
@@ -0,0 +1,26 @@
+$(() => {
+
+    const applyChange = checked => {
+        console.log("CHANGING", { state: checked });
+
+        // save settings:
+        chrome.storage.sync.set({ state: checked });
+
+        // tell content.js to update:
+        chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+            chrome.tabs.sendMessage(tabs[0].id, { state: checked });
+        });
+    };
+
+    $('input#on').on('change', function() {
+        applyChange(this.checked);
+    });
+
+    chrome.storage.sync.get('state', ({ state }) => {
+        let next = state !== undefined ? state : true;
+
+        $('input#on')[0].checked = next;
+        applyChange(next);
+    });
+
+});
